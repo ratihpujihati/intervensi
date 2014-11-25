b413944bb806a11506e373bd8cc9f6d6100cc46e
ratihpujihati <ratih.pujihati@gmail.com>
2014-11-25 12:38:18 +0700

    hitung nii dan warna_indikator

--- etc/modules/indikator/controllers/IndikatorController.php
+++ etc/modules/indikator/controllers/IndikatorController.php
@@ -104,6 +104,7 @@ class Indikator_IndikatorController extends Zend_Controller_Action {
 		
 		$this->view->id_indikator = $id_indikator;
 		$this->view->grafik = $this->menu_serv->getHasil($id_indikator);
+		$this->view->dataHitung = $this->menu_serv->getDataHitung($id_indikator);
     }
 	//------------------------------------------------------------------------END MAXIMUM
 	
