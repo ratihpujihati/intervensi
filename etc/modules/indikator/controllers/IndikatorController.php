7640429c8c22bec6efa08cb0de646de21405c3b4
rizafr <riza.fauzi.rahman@gmail.com>
2014-11-27 14:01:22 +0700

    penambahan nama di indikator grafik

--- etc/modules/indikator/controllers/IndikatorController.php
+++ etc/modules/indikator/controllers/IndikatorController.php
@@ -103,6 +103,7 @@ class Indikator_IndikatorController extends Zend_Controller_Action {
 		$id_indikator = $_REQUEST["id_indikator"];
 		
 		$this->view->id_indikator = $id_indikator;
+		$this->view->namaIndikator = $this->menu_serv->getNamaIndikatorMax($id_indikator);
 		$this->view->grafik = $this->menu_serv->getHasil($id_indikator);
 		$this->view->dataHitung = $this->menu_serv->getDataHitung($id_indikator);
     }
@@ -240,6 +241,7 @@ class Indikator_IndikatorController extends Zend_Controller_Action {
 		$id_indikator = $_REQUEST["id_indikator"];
 		
 		$this->view->id_indikator = $id_indikator;
+		$this->view->namaIndikator = $this->menu_serv->getNamaIndikatorMax($id_indikator);
 		$this->view->grafik = $this->menu_serv->getHasil($id_indikator);
 		$this->view->dataHitung = $this->menu_serv->getDataHitung($id_indikator);
     }
